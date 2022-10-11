53547fa663dcf34ab3955821c7c0eda7f2534455
Vincenzo Caruso <vicaruso@microsoft.com>
2022-10-11 16:10:23 -0700

    updated index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
 import { TileBuilder } from './tileBuilder';
-import type { BaseTile, TileProperties, TileBuilderProperties, TileSize, TileBuilderConfiguration, RenderProperties } from './models';
-
+export type { BaseTile, TileProperties, TileBuilderProperties, TileSize, TileBuilderConfiguration, RenderProperties } from './models';
 export default TileBuilder;
-export type { BaseTile, TileProperties, TileBuilderProperties, TileSize, TileBuilderConfiguration, RenderProperties } 
